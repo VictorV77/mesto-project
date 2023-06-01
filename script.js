83ec8d150bfea2e0fed4c7238ec336911349f067
Victor Voronkov <victorv1986@yandex.ru>
2023-06-01 18:50:05 +0300

    Реализована фича по добавлению карточки места

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const profileEditButton = document.querySelector('.profile-info__edit-button');
-const profileEditPopup = document.querySelector('.edit-profile-popup');
-const profilePopupCloseButton = profileEditPopup.querySelector('.popup__close-icon');
+const userProfilePopup = document.querySelector('.popup_type_user-profile');
+const profilePopupCloseButton = userProfilePopup.querySelector('.popup__close-icon');
 const profileName = document.querySelector('.profile-info__title');
 const profileJob = document.querySelector('.profile-info__subtitle');
-const profileEditNameInput = profileEditPopup.querySelector('.popup__text-input_data_name');
-const profileEditJobInput = profileEditPopup.querySelector('.popup__text-input_data_profession');
-const addPlacePopup = document.querySelector('.add-place-popup');
+const profileEditNameInput = userProfilePopup.querySelector('.popup__text-input_data_name');
+const profileEditJobInput = userProfilePopup.querySelector('.popup__text-input_data_profession');
+const addPlacePopup = document.querySelector('.popup_type_add-place');
 const addPlaceButton = document.querySelector('.profile__add-button');
 const addPlacePopupCloseButton = addPlacePopup.querySelector('.popup__close-icon');
+const cardCatalog = document.querySelector('.places');
 
 profileEditNameInput.value = profileName.textContent;
 profileEditJobInput.value = profileJob.textContent;
 
 function showEditProfilePopup() {
-  profileEditPopup.classList.add('popup_opened');
+  userProfilePopup.classList.add('popup_opened');
 }
 
 function hideEditProfilePopup() {
-  profileEditPopup.classList.remove('popup_opened');
+  userProfilePopup.classList.remove('popup_opened');
 }
 
 function showAddPlacePopup() {
@@ -35,8 +36,24 @@ function handleFormSubmit(evt) {
   hideEditProfilePopup();
 }
 
+function newCardSubmit(evt) {
+  evt.preventDefault();
+  const placeName = addPlacePopup.querySelector('.popup__text-input_data_place-name');
+  const placeLink = addPlacePopup.querySelector('.popup__text-input_data_place-link');
+  const cardTemplate = document.querySelector('#card-template').content;
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  newCard.querySelector('.card__image').src = placeLink.value;
+  newCard.querySelector('.card__image').alt = placeName.value;
+  newCard.querySelector('.card__title').textContent = placeName.value;
+  cardCatalog.prepend(newCard);
+  placeLink.value = ' ';
+  placeName.value = ' ';
+  hideAddPlacePopup();
+}
+
 profileEditButton.addEventListener('click', showEditProfilePopup);
 addPlaceButton.addEventListener('click', showAddPlacePopup);
 profilePopupCloseButton.addEventListener('click', hideEditProfilePopup);
 addPlacePopupCloseButton.addEventListener('click', hideAddPlacePopup);
-profileEditPopup.addEventListener('submit', handleFormSubmit);
+userProfilePopup.addEventListener('submit', handleFormSubmit);
+addPlacePopup.addEventListener('submit', newCardSubmit);
