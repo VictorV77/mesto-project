63d8275e9a854baaad8073bfe6d043d0829bd60d
Victor Voronkov <victorv1986@yandex.ru>
2023-06-03 16:07:46 +0300

    Реализована подгрузка контента карточек из массива

--- script.js
+++ script.js
@@ -11,32 +11,77 @@ const addPlacePopupCloseButton = addPlacePopup.querySelector('.popup__close-icon
 const cardCatalog = document.querySelector('.places');
 const likeButton = cardCatalog.querySelectorAll('.card__like-button');
 const trashButton = cardCatalog.querySelectorAll('.card__trash-button');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 profileEditNameInput.value = profileName.textContent;
 profileEditJobInput.value = profileJob.textContent;
 
+function showCardsFromArray() {
+  initialCards.forEach(function(item) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardFromArray = cardTemplate.querySelector('.card').cloneNode(true);
+    cardFromArray.querySelector('.card__image').src = item.link;
+    cardFromArray.querySelector('.card__image').alt = item.name;
+    cardFromArray.querySelector('.card__title').textContent = item.name;
+    cardFromArray.querySelector('.card__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('card__like-button_active');
+    });
+    cardFromArray.querySelector('.card__trash-button').addEventListener('click', function (evt) {
+      evt.target.closest('.card').remove();
+    });
+    cardCatalog.prepend(cardFromArray);
+  });
+};
+
+showCardsFromArray();
+
 function showEditProfilePopup() {
   userProfilePopup.classList.add('popup_opened');
-}
+};
 
 function hideEditProfilePopup() {
   userProfilePopup.classList.remove('popup_opened');
-}
+};
 
 function showAddPlacePopup() {
   addPlacePopup.classList.add('popup_opened');
-}
+};
 
 function hideAddPlacePopup() {
   addPlacePopup.classList.remove('popup_opened');
-}
+};
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = profileEditNameInput.value;
   profileJob.textContent = profileEditJobInput.value;
   hideEditProfilePopup();
-}
+};
 
 function newCardSubmit(evt) {
   evt.preventDefault();
@@ -57,7 +102,8 @@ function newCardSubmit(evt) {
   placeLink.value = ' ';
   placeName.value = ' ';
   hideAddPlacePopup();
-}
+};
+
 
 profileEditButton.addEventListener('click', showEditProfilePopup);
 addPlaceButton.addEventListener('click', showAddPlacePopup);
@@ -66,17 +112,6 @@ addPlacePopupCloseButton.addEventListener('click', hideAddPlacePopup);
 userProfilePopup.addEventListener('submit', handleFormSubmit);
 addPlacePopup.addEventListener('submit', newCardSubmit);
 
-for (let i = 0; i < likeButton.length; i++) {
-  likeButton[i].addEventListener('click', function (evt) {
-    evt.target.classList.toggle('card__like-button_active');
-  });
-}
-
-for (let i = 0; i < trashButton.length; i++) {
-  trashButton[i].addEventListener('click', function (evt) {
-    evt.target.closest('.card').remove();
-  })
-}
 
 
 
